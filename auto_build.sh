178a87ed58dd26bc62c0e3a0910b0e7ae468e134
Hwiyong <or01020true@gmail.com>
2024-01-30 11:09:56 +0900

    Update auto_build.sh

--- auto_build.sh
+++ auto_build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+current_path=$(pwd)
 webwas_container_name=$1
-
-if [ -z "$webwas_container_name" ]; then
-    echo "오류: 도커 컨테이너 이름을 지정해주세요."
-    echo "사용법: $0 <도커컨테이너이름>"
-    exit 1
+if [ -z "$webwas_container_name" ]; then # not use docker
+    sudo echo "$0 로컬 tomcat 이용"
+    local_tomcat_path=/usr/local/tomcat/apache-tomcat-8.0.53
+    sudo ${local_tomcat_path}/bin/shutdown.sh
+    gradle build
+    sudo cp -f ${current_path}/build/libs/hacking02_sk-0.0.1-SNAPSHOT-plain.war ${local_tomcat_path}/webapps/ROOT.war
+    sudo rm -rf ${local_tomcat_path}/webapps/ROOT
+    sudo ${local_tomcat_path}/bin/startup.sh
+else # use docker
+    echo "$0 Docker tomcat 이용"
+    current_path=$(pwd)
+    docker_tomcat_path=/usr/local/tomcat
+    gradle build
+    docker cp ${current_path}/build/libs/hacking02_sk-0.0.1-SNAPSHOT-plain.war ${webwas_container_name}:${docker_tomcat_path}/webapps/ROOT.war
 fi
-
-gradle build
-sesac_tomcat_path=/usr/local/tomcat
-docker cp ./build/libs/hacking02_sk-0.0.1-SNAPSHOT-plain.war ${webwas_container_name}:${sesac_tomcat_path}/webapps/ROOT.war
