aa52b2c2a5695d6b9d225c83b862f55183ed2349
Hwiyong <or01020true@gmail.com>
2024-02-23 22:14:36 +0900

    Change user balances.

--- mnstsql/myacc_cipher_sample.sql
+++ mnstsql/myacc_cipher_sample.sql
@@ -1,11 +1,11 @@
 INSERT INTO myhacking.myacc (myacc,myid,mymoney,mybank,myaccpw,myaccregdate) VALUES
-	('9geKnEGW39dcJ7Ohg9FQgQ==','jongsu91',30000,'MNST','5d0cb12f8c9ad6845110317afc6e2183','2024-01-25 03:39:22'),
+	('9geKnEGW39dcJ7Ohg9FQgQ==','jongsu91',190000,'MNST','5d0cb12f8c9ad6845110317afc6e2183','2024-01-25 03:39:22'),
 	('jrHz5z5FNtJ9eAqrXcc5yQ==','yunseo19',50000,'MNST','58e16dc760ca166c476403499ff5c59b','2024-01-22 09:54:03'),
 	('iadPt2bs61UVzc8JWaQ3RQ==','nosumin',500000,'MNST','5e9167571c4fdf5f5bceabb11ca7d4c9','2024-01-28 08:04:52'),
-	('GJn7IfRHKA/xTttSYg3vyA==','mgweon',7000000,'MNST','22c432f46fd86e1be5bc4429282eb65d','2024-01-17 18:02:32'),
+	('GJn7IfRHKA/xTttSYg3vyA==','mgweon',27000000,'MNST','22c432f46fd86e1be5bc4429282eb65d','2024-01-17 18:02:32'),
 	('kOzZUpn0shVMzjyB6QxXAQ==','gimyeeun',250000,'MNST','4dc3ed26a29c9c3df3ec373524377a5b','2024-02-07 02:11:05'),
 	('Pwl1mHuTN2S0jB6Q9qnYZw==','haeun99',40000000,'MNST','0b4078ee99ffc88fb383ffcdcd377d67','2024-01-29 13:13:48'),
 	('AyOwr/QJbNvKNZEmnAZE6w==','yejun14',3000000,'MNST','b20bb95ab626d93fd976af958fbc61ba','2024-01-26 01:33:37'),
 	('MEGxQQHEuAdHeAHpEWPnog==','jiminnam',2000000,'MNST','3a029f04d76d32e79367c4b3255dda4d','2024-01-24 22:53:40'),
-	('6XPPeFJ3pjQx/k3oZGiGQQ==','yeongja88',190000,'MNST','2990cb071c6413d6cde1512f6474506a','2024-01-20 18:48:18'),
+	('6XPPeFJ3pjQx/k3oZGiGQQ==','yeongja88',90000,'MNST','2990cb071c6413d6cde1512f6474506a','2024-01-20 18:48:18'),
 	('egVGfJCwwkez5LOY/yCTHQ==','administ',2100000000,'MNST','cc1d60fc967b66b66229205d7bb288e8','2023-02-20 14:30:40');
